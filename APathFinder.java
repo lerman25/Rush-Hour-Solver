ae58f2c80ad42d895ccd529d26b8a32004f1d16c
OmerLerman <lerman25@gmail.com>
2020-04-25 21:57:30 +0300

    compare

--- APathFinder.java
+++ APathFinder.java
@@ -1,78 +1,77 @@
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 
 public class APathFinder {
 
-	public boolean aStarSearch()
-	{
-		Board startNode= new Board();
-		startNode.readBoard_scan();
-		// Board endNode = ;
-		//change to the goal , we have to define how it is defined
-		 HashMap<Board,Board>  parentMap =new HashMap<Board,Board>();
-		HashSet<Board> visited = new HashSet<Board>();
-		 double inf = Double.POSITIVE_INFINITY;
-		 Map<Board, Double> distances = null ;
-	    //to implement initializeAllToInfinity();
-		 Queue<Board> priorityQueue = null;
-		 // ** enque StartNode, with distance 0**
-	       startNode.setDistanceToStart(new Double(0));
-	       distances.put(startNode, new Double(0));
-	      priorityQueue.add(startNode);
-	      Board current = null;
-	      while (!priorityQueue.isEmpty()) {
-	    	  current = priorityQueue.remove();
-	    	  
-	    	  if (!visited.contains(current) ){
-	                visited.add(current);
-	                // if last element in PQ reached
-	                if (false)
-	                { // need to check here how to check if the red car is in the goal spot 
-	                	return true;
-	                
-	                    }
+	public boolean aStarSearch() {
+		Node startNode = new Node(0);
+		// change to the goal , we have to define how it is defined
+		HashMap<Node, Node> parentMap = new HashMap<Node, Node>();
+		HashSet<Node> visited = new HashSet<Node>();
+		double inf = Double.POSITIVE_INFINITY;
+		PriorityQueue<Node> pQueue = new PriorityQueue<Node>(new Comparator<Node>() {
+			@Override
+			public int compare(Node o1, Node o2) {
+				// TODO Auto-generated method stub
+				if(o1.getF_grade()==o2.getF_grade())
+				return 0;
+				if(o1.getF_grade()>o2.getF_grade())
+				return 1;
+				if(o1.getF_grade()<o2.getF_grade())
+				return -1;
+			}
+		});
+		// ** enque StartNode, with distance 0**
+		startNode.setDistanceToStart(new Double(0));
+		distances.put(startNode, new Double(0));
+		priorityQueue.add(startNode);
+		Node current = null;
+		while (!priorityQueue.isEmpty()) {
+			current = priorityQueue.remove();
 
-	                Set<Board> neighbors = getNeighbors(current);
-	                for (Board neighbor : neighbors) {
-	                    if (!visited.contains(neighbor) ){  
-	 
-	                        // calculate predicted distance to the end node**hueristic computation
-	                       // double predictedDistance = neighbor.getDistance();
-	 
-	                        // 1. calculate distance to neighbor. 2. calculate dist from start node
-	                        double neighborDistance = neighbor.getDistanceToStart();
-	                        // double totalDistance = current.getDistance() + neighborDistance + predictedDistance;
-	                        double totalDistance = current.getDistanceToStart()+neighborDistance;
-	                        // check if distance smaller
-	                        if(totalDistance < distances.get(neighbor) ){
-	                            // update n's distance
-	                            distances.put(neighbor, totalDistance);
-	                            // used for PriorityQueue
-	                            neighbor.setDistanceToStart(totalDistance);
-	                           // neighbor.setPredictedDistance(predictedDistance);
-	                            // set parent
-	                            parentMap.put(neighbor, current);
-	                            // enqueue
-	                            priorityQueue.add(neighbor);
-	                        }
-	                    } 
-			        }
-               }
-          }
-	    	  
-	    	  
-	    	  return false;
+			if (!visited.contains(current)) {
+				visited.add(current);
+				// if last element in PQ reached
+				if (false) { // need to check here how to check if the red car is in the goal spot
+					return true;
+				}
+				Set<Node> neighbors = getNeighbors(current);
+				for (Node neighbor : neighbors) {
+					if (!visited.contains(neighbor)) {
+						
+
+						// calculate predicted distance to the end node**hueristic computation
+						// double predictedDistance = neighbor.getDistance();
+
+						// 1. calculate distance to neighbor. 2. calculate dist from start node
+						int neighborDistance = neighbor.getDistance();
+						// double totalDistance = current.getDistance() + neighborDistance +
+						// predictedDistance;
+						int totalDistance = current.getDistance() + neighborDistance;
+						// check if distance smaller
+						if (totalDistance < distances.get(neighbor)) {
+							// update n's distance
+							distances.put(neighbor, totalDistance);
+							// used for PriorityQueue
+							neighbor.setDistance(totalDistance);
+							// neighbor.setPredictedDistance(predictedDistance);
+							// set parent
+							parentMap.put(neighbor, current);
+							// enqueue
+							priorityQueue.add(neighbor);
+						}
+					}
+				}
+			}
+		}
+
+		return false;
 	}
 
-	private Set<Board> getNeighbors(Board current) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	
-	
-	
 }
---
+++ Node.java
@@ -0,0 +1,76 @@
+import java.util.LinkedList;
+
+public class Node {
+	private Board board;
+	private int distance;
+	private float f_grade;
+	private LinkedList<Node> neighbors;
+	public Node(int _distance)
+	{
+		board = new Board();
+		board.readBoard_scan();
+		distance=_distance;
+		f_grade = calcGrade();
+		neighbors = new LinkedList<Node>();
+	}
+	public void add_n(Node neighbor)
+	{
+		neighbors.add(neighbor);
+	}
+	public void add_ns(LinkedList<Node> _neighbors)
+	{
+		neighbors.addAll(_neighbors);
+	}
+	public LinkedList<Node> getNeighbors() {
+		return neighbors;
+	}
+	public void setNeighbors(LinkedList<Node> neighbors) {
+		this.neighbors = neighbors;
+	}
+	public float calcGrade()
+	{
+		return board.h()+distance;
+	}
+	public Board getBoard() {
+		return board;
+	}
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+	public int getDistance() {
+		return distance;
+	}
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+	public float getF_grade() {
+		return f_grade;
+	}
+	public void setF_grade(float f_grade) {
+		this.f_grade = f_grade;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((board == null) ? 0 : board.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node other = (Node) obj;
+		if (board == null) {
+			if (other.board != null)
+				return false;
+		} else if (!board.equals(other.board))
+			return false;
+		return true;
+	}
+
+}
